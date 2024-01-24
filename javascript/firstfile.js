435811c237f791b2484b442063153a39f096e288
Ismail <lord28062000@gmail.com>
2024-01-25 00:35:48 +0500

    lec 2 js

--- javascript/firstfile.js
+++ javascript/firstfile.js
@@ -1,1 +1,49 @@
-alert("Hello World!")
+// alert("Hello World!")
+
+let x;
+x = 3; // number
+let y = "Ismail"; // string, text string
+
+x = 33
+// let y = "JOHN" // can't redeclare a let variable.
+
+var xy = 3;
+var xy = 45;
+
+let sum;
+
+const I = 3;
+
+
+let constSum;
+{
+    constSum = I + 4;
+    const value1 = 45;
+    console.log(value1);
+    sum = 56 + 67; //123
+    let subtract = 30;
+    // console.log(subtract)
+    // console.log(sum)
+}
+const value1 = 66;
+
+sum = sum + 3;
+// console.log(sum);
+
+
+let subtract = 3;
+
+// console.log(ismail);
+// var ismail = 3;
+
+var ismail = 3;
+console.log(ismail);
+
+
+let ismail1 = 4;
+console.log(ismail1);
+
+
+// const smash1;
+//  smash1 = 2
+
