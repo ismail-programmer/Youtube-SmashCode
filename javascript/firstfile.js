05a5f25f076c56980ad3d66a3200a4b7ae94724d
Ismail <lord28062000@gmail.com>
2024-05-11 22:43:39 +0500

    lec 17 js

--- javascript/firstfile.js
+++ javascript/firstfile.js
@@ -487,8 +487,64 @@ function add3(a1, a2) {
 
 console.log(add3(2, 4));
 
+// function expression
+
+let addition = function (x, y, z) {
+    let sum = x + y + z;
+    return sum;
+}
+
+console.log(addition(4, 5, 6));
+
+// arrow function with one line
+let subtract3 = (input1, input2) => input1 - input2;
+console.log(subtract3(50, 30));
+
+let result332 = 39;
+// arrow function with more than one line
+let subtract4 = (input1, input2) => {
+    let result456 = input1 - input2;
+    console.log("this is local function variable witin the function " + result456)
+    console.log("this is global  variable witin the function " + result332)
+
+    return result456
+}
+
+// console.log(subtract4(40, 30));
+subtract4(40, 30) // function call 
+// console.log("Local varibale outside of function" + result456);
+console.log("this is global  variable outside of the function " + result332)
 
 
+// function example 1
+// function to calculate the sum of numbers from 1 to n
+
+let calculateSum = (n) => {
+    let sum = 0;
+
+// loop from 1 to n and add each number to sum
+    for (let i = 1; i <= n; i++) {
+        sum += i
+    }
+    return sum;
+}
+
+const result123 = calculateSum(10);
+console.log("Sum of numbers from 1 to 10 is " + result123);
+console.log("Sum of numbers from 1 to 100 is " + calculateSum(100));
+console.log("Sum of numbers from 1 to 1000 is " + calculateSum(1000));
 
 
+//function example 2
+// function to count the number of even  number in an array.
 
+let countEvenNumber = (arr) => {
+    let count = 0;
+    for (let num of arr) {
+        if (num % 2 === 0) count++;
+    }
+    return count;
+}
+
+const numbers3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+console.log("Number of even numbers in the array are ", countEvenNumber(numbers3));
